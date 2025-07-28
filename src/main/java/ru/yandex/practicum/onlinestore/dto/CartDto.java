5d259becec28a8bd9458404199a24b6caf6fb4f3
AlexGin333 <54118236+UserNameToHub@users.noreply.github.com>
2025-07-28 18:43:46 +0300

    added CartDto

---
+++ src/main/java/ru/yandex/practicum/onlinestore/dto/CartDto.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.onlinestore.dto;
+
+import lombok.Data;
+import ru.yandex.practicum.onlinestore.entity.Item;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class CartDto {
+    private List<Item> items;
+
+    private BigDecimal total;
+
+    private Boolean isEmpty;
+}
