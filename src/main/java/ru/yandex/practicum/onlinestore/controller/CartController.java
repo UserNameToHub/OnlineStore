d217e8160384655b8ad3fa647c80d29d31e4958f
AlexGin333 <54118236+UserNameToHub@users.noreply.github.com>
2025-08-24 01:19:44 +0300

    added controllers

--- src/main/java/ru/yandex/practicum/onlinestore/controller/CartController.java
+++ src/main/java/ru/yandex/practicum/onlinestore/controller/CartController.java
@@ -1,20 +1,33 @@
 package ru.yandex.practicum.onlinestore.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.onlinestore.dto.CartDto;
-import ru.yandex.practicum.onlinestore.enumiration.ActionConstant;
+import ru.yandex.practicum.onlinestore.service.CartService;
+import ru.yandex.practicum.onlinestore.util.Util;
 
 @Controller
 @RequestMapping("/cart/items")
+@RequiredArgsConstructor
 public class CartController {
+
+    private final CartService cartService;
+
     @GetMapping
-    public String get(@ModelAttribute CartDto cartDto) {
+    public String get(Model model) {
+        CartDto itemsInCart = cartService.getAll();
+        model.addAttribute("items", itemsInCart.getItems());
+        model.addAttribute("total", itemsInCart.getTotal());
+        model.addAttribute("empty", itemsInCart.getIsEmpty());
+
         return "cart";
     }
 
-    @PostMapping
-    public String edit(@RequestParam(value = "action")ActionConstant action) {
-        return "redirect:/main/items";
+    @PostMapping("/{id}")
+    public String edit(@PathVariable("id") Long id, @RequestParam(value = "action") String action) {
+        cartService.update(id, Util.getAction(action));
+        return "redirect:/cart/items";
     }
 }
--- src/main/java/ru/yandex/practicum/onlinestore/controller/MainController.java
+++ src/main/java/ru/yandex/practicum/onlinestore/controller/MainController.java
@@ -2,33 +2,50 @@ package ru.yandex.practicum.onlinestore.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.onlinestore.enumiration.ActionConstant;
+import ru.yandex.practicum.onlinestore.entity.Item;
 import ru.yandex.practicum.onlinestore.enumiration.SortConstant;
+import ru.yandex.practicum.onlinestore.mapper.PagingMapper;
+import ru.yandex.practicum.onlinestore.service.ItemService;
+import ru.yandex.practicum.onlinestore.util.Util;
+
+import java.util.List;
 
 @Slf4j
 @Controller
-@RequestMapping
 @RequiredArgsConstructor
 public class MainController {
+    private final ItemService itemService;
+
+    private final PagingMapper pagingMapper;
 
     @GetMapping("/")
+    @ResponseStatus(HttpStatus.PERMANENT_REDIRECT)
     public String getMain() {
         return "redirect:/main/items";
     }
 
-    @GetMapping("/main/items")
+    @GetMapping(value = "/main/items", produces = MediaType.TEXT_HTML_VALUE)
     public String get(@RequestParam(value = "search", defaultValue = "") String search,
                       @RequestParam(value = "sort", defaultValue = "NO") SortConstant sort,
                       @RequestParam(value = "pageNumber", defaultValue = "1") Integer pageNumber,
-                      @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize) {
+                      @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
+                      Model model) {
+        List<Item> items = itemService.getAll(search, sort, pageSize, pageNumber);
+        model.addAttribute("items", items);
+        model.addAttribute("paging", pagingMapper.toPagingDto(pageNumber, pageSize, items.size()));
+
         return "main";
     }
 
     @PostMapping("/main/items/{id}")
     public String edit(@PathVariable(value = "id") Long id,
-                       @RequestParam(value = "cation") ActionConstant action) {
+                       @RequestParam(value = "action") String action) {
+        itemService.update(id, Util.getAction(action));
         return "redirect:/main/items";
     }
 }
