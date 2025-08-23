2e0b9fc06de0bdafd86c51edfe9953499a696e6b
AlexGin333 <54118236+UserNameToHub@users.noreply.github.com>
2025-08-24 01:31:20 +0300

    added util

--- src/main/java/ru/yandex/practicum/onlinestore/util/Constants.java
+++ src/main/java/ru/yandex/practicum/onlinestore/util/Constants.java
@@ -1,5 +1,11 @@
 package ru.yandex.practicum.onlinestore.util;
 
+import org.springframework.data.domain.Sort;
+
 public class Constants {
     public static final String UPLOAD_DIR = "C:/store/uploads/";
+
+    public static final Sort SORT_BY_PRICE = Sort.by(Sort.Direction.ASC, "price");
+
+    public static final Sort SORT_BY_ALPHA = Sort.by(Sort.Direction.ASC, "title");
 }
