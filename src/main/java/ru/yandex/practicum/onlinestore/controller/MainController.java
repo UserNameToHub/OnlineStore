ee1ebb383848d7837309b21b7ed8b48cc1550009
AlexGin333 <54118236+UserNameToHub@users.noreply.github.com>
2025-09-15 02:02:04 +0300

    added r2dbc main controller

--- src/main/java/ru/yandex/practicum/onlinestore/controller/MainController.java
+++ src/main/java/ru/yandex/practicum/onlinestore/controller/MainController.java
@@ -7,13 +7,11 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.onlinestore.entity.Item;
+import reactor.core.publisher.Mono;
 import ru.yandex.practicum.onlinestore.enumiration.SortConstant;
 import ru.yandex.practicum.onlinestore.mapper.PagingMapper;
 import ru.yandex.practicum.onlinestore.service.ItemService;
 import ru.yandex.practicum.onlinestore.util.Util;
-
-import java.util.List;
 
 @Slf4j
 @Controller
@@ -25,27 +23,29 @@ public class MainController {
 
     @GetMapping("/")
     @ResponseStatus(HttpStatus.PERMANENT_REDIRECT)
-    public String getMain() {
-        return "redirect:/main/items";
+    public Mono<String> getMain() {
+        return Mono.just("redirect:/main/items");
     }
 
     @GetMapping(value = "/main/items", produces = MediaType.TEXT_HTML_VALUE)
-    public String get(@RequestParam(value = "search", defaultValue = "") String search,
+    public Mono<String> get(@RequestParam(value = "search", defaultValue = "") String search,
                       @RequestParam(value = "sort", defaultValue = "NO") SortConstant sort,
                       @RequestParam(value = "pageNumber", defaultValue = "1") Integer pageNumber,
                       @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
                       Model model) {
-        List<Item> items = itemService.getAll(search, sort, pageSize, pageNumber);
-        model.addAttribute("items", items);
-        model.addAttribute("paging", pagingMapper.toPagingDto(pageNumber, pageSize, items.size()));
-
-        return "main";
+        return itemService.getAll(search, sort, pageSize, pageNumber)
+                .collectList()
+                .doOnNext(items -> {
+                    model.addAttribute("items", items);
+                    model.addAttribute("paging", pagingMapper.toPagingDto(pageNumber, pageSize, items.size()));
+                })
+                .thenReturn("main");
     }
 
     @PostMapping("/main/items/{id}")
-    public String edit(@PathVariable(value = "id") Long id,
+    public Mono<String> edit(@PathVariable(value = "id") Long id,
                        @RequestParam(value = "action") String action) {
-        itemService.update(id, Util.getAction(action));
-        return "redirect:/main/items";
+        return itemService.update(id, Util.getAction(action))
+                .thenReturn("redirect:/main/items");
     }
 }
