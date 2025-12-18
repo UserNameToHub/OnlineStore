eb52ab9744bae02fb3c5b29cdfd13c8eaa5fa6da
AlexGin333 <54118236+UserNameToHub@users.noreply.github.com>
2025-12-19 00:10:47 +0300

    add client

---
+++ Showcase/src/main/java/ru/yandex/practicum/onlinestore/confiquration/ClientConfiguration.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.onlinestore.confiquration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import ru.yandex.practicum.onlinestore.client.StoreClient;
+
+@Configuration
+@RequiredArgsConstructor
+public class ClientConfiguration {
+    @Value(value = "${payment.url}")
+    private final String url;
+
+    @Bean
+    RestTemplate paymentRestTemplate() {
+        return new RestTemplateBuilder().rootUri(url).build();
+    }
+}
