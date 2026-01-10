07006cbf42041221cbb0f0759a5a4bd216cd4a08
AlexGin333 <54118236+UserNameToHub@users.noreply.github.com>
2026-01-10 23:50:12 +0300

    change schema.sql

--- Showcase/src/main/resources/schema.sql
+++ Showcase/src/main/resources/schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS item
 
 CREATE TABLE IF NOT EXISTS orders
 (
-    Id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+    Id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT REFERENCES user (id)  NOT NULL -- + принадлежность заказа к пользователю
 );
 
 CREATE TABLE IF NOT EXISTS order_item
@@ -25,6 +26,27 @@ CREATE TABLE IF NOT EXISTS order_item
 CREATE TABLE IF NOT EXISTS cart
 (
     Id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    item_id  BIGINT REFERENCES item (id)  NOT NULL
+    item_id  BIGINT REFERENCES item (id)  NOT NULL,
+    user_id BIGINT REFERENCES user (id)  NOT NULL, -- + принадлежность к пользователю
+    order_id BIGINT REFERENCES orders (id) -- + принадлежность к заказу, чтобы определить актуальную корзину пользователя
+);
 
+CREATE TABLE IF NOT EXISTS user
+(
+    Id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username VARCHAR(250) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS role
+(
+    Id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    role VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS user_role
+(
+    user_id INTEGER REFERENCES user (id) NOT NULL,
+    role_id  INTEGER REFERENCES role (id)  NOT NULL,
+    PRIMARY KEY (user_id, role_id)
 );
