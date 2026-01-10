06886054aade0e6e929b148ca5a850800c94101d
AlexGin333 <54118236+UserNameToHub@users.noreply.github.com>
2026-01-10 23:48:21 +0300

    added change and new services

---
+++ Showcase/src/main/java/ru/yandex/practicum/onlinestore/service/UserService.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.onlinestore.service;
+
+import ru.yandex.practicum.onlinestore.common.CrudService;
+import ru.yandex.practicum.onlinestore.dto.UserDto;
+
+public interface UserService extends CrudService<UserDto, Long> {
+    //TODO Add methods that are characteristic of this service
+}
